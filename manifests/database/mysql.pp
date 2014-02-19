0583d23c7c85c781799d0fac77b0d41b60b258b6
Robbie Burda <robbieb@slalom.com>
2014-02-18 20:00:11 -0600

    minor tweaks to mysql

--- manifests/database/mysql.pp
+++ manifests/database/mysql.pp
@@ -36,15 +36,15 @@ class kegbot::database::mysql inherits kegbot::database {
 
     exec {
         'set_root_pwd':
-            command     => "mysqladmin -u ${::kegbot::database::db_root_usr} password '${::kegbot::database::db_root_pwd}'",
+            command     => "mysqladmin --user=${::kegbot::database::db_root_usr} password ${::kegbot::database::db_root_pwd}",
             subscribe   => Package['mysql-server'],
             refreshonly => true;
         'create_kegbot_db':
-            command => "mysql -u${::kegbot::database::db_root_usr} -p${::kegbot::database::db_root_pwd} -e 'create database kegbot;' -sN",
-            onlyif  => "test `mysql -uroot -p${::kegbot::database::db_root_pwd} -e 'show databases;' -sN | grep -c '^kegbot$'` -eq 0";
+            command => "mysql --user=${::kegbot::database::db_root_usr} --password=${::kegbot::database::db_root_pwd} -e 'create database kegbot;' -sN",
+            onlyif  => "test \$(mysql --user=${::kegbot::database::db_root_usr} --password=${::kegbot::database::db_root_pwd} -e 'show databases;' -sN | grep -c 'kegbot') -eq 0";
         'create_kegbot_db_user':
-            command => "mysql -u${::kegbot::database::db_root_usr} -p${::kegbot::database::db_root_pwd} -e 'GRANT ALL PRIVILEGES ON kegbot.* to ${::kegbot::kegbot_usr}@localhost IDENTIFIED BY \"${::kegbot::kegbot_pwd}\";' -sN",
-            unless  => "mysql -u${::kegbot::kegbot_usr} -p${::kegbot::kegbot_pwd} kegbot -e 'show tables;'";
+            command => "mysql --user=${::kegbot::database::db_root_usr} --password=${::kegbot::database::db_root_pwd} -e \"GRANT ALL PRIVILEGES ON kegbot.* to ${::kegbot::kegbot_usr}@localhost IDENTIFIED BY '${::kegbot::kegbot_pwd}';\" -sN",
+            unless  => "mysql --user=${::kegbot::kegbot_usr} --password=${::kegbot::kegbot_pwd} kegbot -e 'show tables;'";
     }
 
     Service['mysql'] ->
