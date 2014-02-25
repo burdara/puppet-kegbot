a3265038b84f3b8b3610bc310ded45220ac3703c
Robbie Burda <robbieb@slalom.com>
2014-02-24 18:08:08 -0600

    that should do it for mysql fixes...

--- manifests/install/github.pp
+++ manifests/install/github.pp
@@ -17,13 +17,14 @@
 # Tyler Walters <github.com/tylerwalts>
 #
 class kegbot::install::github inherits kegbot::install {
+    $repo_dir = "${::kegbot::install_dir}/repo"
     $github_repo = 'https://github.com/Kegbot/kegbot.git'
     $source_env_activate = "source ${::kegbot::install_dir}/bin/activate"
 
-    $git_clone = "git clone ${github_repo} ${::kegbot::install_dir}/repo"
+    $git_clone = "git clone ${github_repo} ${repo_dir}"
     $clone_repo_command = "bash -c '${source_env_activate} && ${git_clone}'"
 
-    $repo_setup = "${::kegbot::install_dir}/repo/setup.py install"
+    $repo_setup = "cd ${repo_dir} && ./setup.py --quiet install"
     $setup_repo_command = "bash -c '${source_env_activate} && ${repo_setup}'"
 
     exec {
@@ -33,7 +34,7 @@ class kegbot::install::github inherits kegbot::install {
             timeout => 600;
         'setup_repo':
             command => $setup_repo_command,
-            creates => "${::kegbot::install_dir}/repo/bin/kegbot";
+            creates => "${::kegbot::install_dir}/bin/kegbot";
     }
 
     Exec['clone_repo'] ->
